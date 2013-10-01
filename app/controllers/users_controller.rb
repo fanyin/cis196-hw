acf6802a6f218d176bc2b810664ab9d22854a798
Fan Yin <fanyin1234@gmail.com>
2013-10-01 06:55:40 -0400

    updated user_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,13 +4,19 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(params.require(:user))
+    @user = User.new(user_params)
+    
     if @user.save
         redirect_to users_path
     else
         render 'new'
     end
   end
+         
+  private
+    def user_params
+        params.require(:user).permit(:name, :email, :password)
+    end
 
   def index
     @users = User.all
