f8244573414282e390d3af7887fe868930aa6768
Fan Yin <fanyin1234@gmail.com>
2013-10-07 07:49:28 -0400

    add new app files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Site::Application.config.secret_token = 'c7208df8619d630c5c4c55f05a7370713c29da07059d8825ee014798ec9dcd3cb3e2cdfebdf8297b530ddd9a49f25a0f88fc5877ae1858adbf1694e4b54e563d'
