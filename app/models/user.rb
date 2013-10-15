5e7c57aaf85dcb69d5bcf47c22da53e4dec703f4
Fan Yin <fanyin1234@gmail.com>
2013-10-15 06:53:44 -0400

    added validation to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :name, :password
+  validates :name, presence: true
+  validates :password, length: {minimum: 6}, presence: true
+  emailFormatValidation = /[a-z.-]+@[a-z\d.-]+[.][a-z]+/i
+  validates :email, format: {with: emailFormatValidation}, uniqueness: true
 end
